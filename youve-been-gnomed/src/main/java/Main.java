e0b5e32894f670ebc0e799f222609f79b3e8be7b
Andrew Peterson <andrewp2@iastate.edu>
2019-02-04 23:14:06 -0600

    Change from Scanner+File to BufferedReader+InputStream to make it find the token file in the jar for deployment

--- youve-been-gnomed/src/main/java/Main.java
+++ youve-been-gnomed/src/main/java/Main.java
@@ -2,35 +2,37 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.internal.requests.Route;
 
 import javax.security.auth.login.LoginException;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FilenameFilter;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class Main extends ListenerAdapter {
+    public static long lastUsed = 0L;
     public static void main(String[] args) throws LoginException {
+        lastUsed = System.currentTimeMillis();
         JDABuilder builder = new JDABuilder(AccountType.BOT);
+        InputStream source = Main.class.getClassLoader().getResourceAsStream("token");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(source));
+        String token = null;
         try {
-            Scanner scan = new Scanner(new File("./youve-been-gnomed/src/main/resources/token"));
-            String token = scan.nextLine();
-            System.out.println(token);
-            builder.setToken(token);
-            builder.addEventListeners(new Main());
-            builder.build();
-        } catch (FileNotFoundException e) {
+            token = bf.readLine();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        builder.setToken(token);
+        builder.addEventListeners(new Main());
+        builder.build();
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        //System.out.println("We received a message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
-        if(event.getMessage().getContentRaw().equals("!gnomePlay")) {
+        if(event.getMessage().getContentRaw().equals("!gnomePlay") && lastUsed + 3000 < System.currentTimeMillis()) {
+            lastUsed = System.currentTimeMillis();
             Board board = new Board(10, 10);
             board.generate(12);
             event.getChannel().sendMessage(board.toDiscordString()).queue();
